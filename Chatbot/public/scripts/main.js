50e70e140d6e35fbaa6a7e3949473c41e15aba77
mussersl <58488336+mussersl@users.noreply.github.com>
2020-11-04 12:24:38 -0500

    Added documentation and enter key functionality

--- Chatbot/public/scripts/main.js
+++ Chatbot/public/scripts/main.js
@@ -19,14 +19,16 @@ class speech {
 }
 
 /* Main */
-/** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
 	rhit.chatList.push(new speech("Hello", rhit.id.BOT));
 	rhit.displayFullChatLog();
 	$("#ask-button").click(rhit.askButton);
+	$("#questioninput").keyup(rhit.enterInput);
 };
 
+
+// prints entire chat log into text box
 rhit.displayFullChatLog = function () {
 	rhit.chatList.forEach(dialogue => {
 		if(dialogue.textId == rhit.id.BOT){
@@ -37,8 +39,11 @@ rhit.displayFullChatLog = function () {
 	});
 }
 
+// display last dialogue in the chatList
 rhit.displayLast = function () {
 	let last = rhit.chatList[rhit.chatList.length - 1];
+
+	// Assigns class text for display purposes
 	if(last.textId == rhit.id.BOT){
 		$("#chatlog").append("<div class=\"bot-text\">"+last.text+"</div>");
 	} else {
@@ -46,11 +51,36 @@ rhit.displayLast = function () {
 	}
 }
 
+// Recognizes enter key press
+rhit.enterInput = function (e) {
+	if(e.which == 13){
+		rhit.askButton();
+	}
+}
+
+
+// Function attacched to ask button
 rhit.askButton = function() {
 	let question = document.getElementById("questioninput").value;
+
+	// add question to log
 	rhit.chatList.push(new speech(question, rhit.id.USER));
+	rhit.displayLast();
+
+	// clear question from text input
+	document.getElementById("questioninput").value = "";
+
+	// Query chatBot
+	let response = rhit.chatBotAskQuestion(question);
+
+	// add response to log
+	rhit.chatList.push(new speech(response, rhit.id.BOT));
 	rhit.displayLast();
 }
 
+// Calls chatbot code and returns chatbot response
+rhit.chatBotAskQuestion = function (question) {
+	return "Question recieved: " + question;
+}
 
 rhit.main();
