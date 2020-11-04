45a0d1073638a362670dac67d9a09fb62b9aee19
mussersl <58488336+mussersl@users.noreply.github.com>
2020-11-04 10:25:10 -0500

    Formated text

--- Chatbot/public/scripts/main.js
+++ Chatbot/public/scripts/main.js
@@ -1,12 +1,5 @@
-/**
- * @fileoverview
- * Provides the JavaScript interactions for all pages.
- *
- * @author 
- * PUT_YOUR_NAME_HERE
- */
 
-/** namespace. */
+
 var rhit = rhit || {};
 
 /** globals */
