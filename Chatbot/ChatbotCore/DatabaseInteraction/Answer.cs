c3c6a68349c4eeb385efab88b2efc9a5faeb200a
mussersl <58488336+mussersl@users.noreply.github.com>
2020-12-15 15:23:55 -0500

    Renamed folder

--- Chatbot/ChatbotCore/DatabaseInteraction/Answer.cs
+++ Chatbot/ChatbotCore/DatabaseInteraction/Answer.cs
@@ -5,10 +5,13 @@ namespace Chatbot
 {
     public class Answer
     {
+        //retrieved from database
         public String ID { get; set; }
         public String question { get; set; }
         public String answer { get; set; }
-        public Dictionary<String, double> keywords { get; set; }
+        public Dictionary<String, double> keywords { get; set; }  //Keyword, relevency to question
+
+        //Assigned by relevencyAnlyzer
         public double relevency { get; set; }
     }
 }
