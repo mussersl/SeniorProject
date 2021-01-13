710ae8a37ce7555ab876faa39a6cb6f1bb2b80b5
mussersl <58488336+mussersl@users.noreply.github.com>
2021-01-13 01:13:04 -0500

    Updated Test.cs

--- Chatbot/ChatbotCore/Test/Test.cs
+++ Chatbot/ChatbotCore/Test/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Chatbot;
+using System.Collections.Generic;
 
 namespace Chatbot
 {
@@ -14,7 +15,25 @@ namespace Chatbot
                 string question = Console.ReadLine();
                 string answer = control.askQuestion(question);
                 Console.WriteLine(answer);
-                Console.WriteLine("Now running test on predetermined Answer");
+                Console.WriteLine("Now running test on predetermined Question.");
+                Answer ans = new Answer();
+                ans.ID = "0001";
+                ans.question = "What does IRPA stand for?";
+                ans.answer = "IRPA stands for Institutional Research Planning and Assessment.";
+                ans.keywords = new System.Collections.Generic.Dictionary<string, double>();
+                ans.keywords.Add("what", 0.7);
+                ans.keywords.Add("IRPA", 0.4);
+                ans.keywords.Add("stand", 0.8);
+                ans.relevency = 0.8;
+                List<Answer> listAns = new List<Answer>();
+                listAns.Add(ans);
+                ResponseGeneratorInterface rg = new ResponseGeneratorR1();
+                List<string> response = rg.generateResponse(listAns);
+                Console.WriteLine(ans.question);
+                foreach(string an in response)
+                {
+                    Console.WriteLine(an);
+                };
                 
             }
         }
