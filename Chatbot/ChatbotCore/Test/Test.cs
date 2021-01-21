642aabdba693772a7ad68a37542348cdf856d5db
mussersl <58488336+mussersl@users.noreply.github.com>
2021-01-21 14:57:01 -0500

    Updated test

--- Chatbot/ChatbotCore/Test/Test.cs
+++ Chatbot/ChatbotCore/Test/Test.cs
@@ -9,32 +9,35 @@ namespace Chatbot
         static void Main(string[] args)
         {
             ControlFlow control = new ControlFlow();
+            Console.WriteLine("Welcome to the IRPA chatbot.");
             while (true)
             {
-                Console.WriteLine("Welcome to the IRPA chatbot.");
+                Console.WriteLine("Please ask a question.");
                 string question = Console.ReadLine();
                 string answer = control.askQuestion(question);
                 Console.WriteLine(answer);
-                Console.WriteLine("Now running test on predetermined Question.");
-                Answer ans = new Answer();
-                ans.ID = "0001";
-                ans.question = "What does IRPA stand for?";
-                ans.answer = "IRPA stands for Institutional Research Planning and Assessment.";
-                ans.keywords = new System.Collections.Generic.Dictionary<string, double>();
-                ans.keywords.Add("what", 0.7);
-                ans.keywords.Add("IRPA", 0.4);
-                ans.keywords.Add("stand", 0.8);
-                ans.relevency = 0.8;
-                List<Answer> listAns = new List<Answer>();
-                listAns.Add(ans);
-                ResponseGeneratorInterface rg = new ResponseGeneratorR1();
-                List<string> response = rg.generateResponse(listAns);
-                Console.WriteLine(ans.question);
-                foreach(string an in response)
+                //Console.WriteLine("Now running test on predetermined Question.");
+                if (question.Equals("What does IRPA stand for?"))
                 {
-                    Console.WriteLine(an);
-                };
-                
+                    Answer ans = new Answer();
+                    ans.ID = "0001";
+                    ans.question = "What does IRPA stand for?";
+                    ans.answer = "IRPA stands for Institutional Research Planning and Assessment.";
+                    ans.keywords = new System.Collections.Generic.Dictionary<string, double>();
+                    ans.keywords.Add("what", 0.7);
+                    ans.keywords.Add("IRPA", 0.4);
+                    ans.keywords.Add("stand", 0.8);
+                    ans.relevency = 0.8;
+                    List<Answer> listAns = new List<Answer>();
+                    listAns.Add(ans);
+                    ResponseGeneratorInterface rg = new ResponseGeneratorR1();
+                    List<string> response = rg.generateResponse(listAns);
+                    Console.WriteLine(ans.question);
+                    foreach (string an in response)
+                    {
+                        Console.WriteLine(an);
+                    };
+                }
             }
         }
     }
