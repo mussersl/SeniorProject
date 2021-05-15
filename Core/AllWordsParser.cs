7a7c8a5ce3f4891bc09cb50461c76d08e5193571
mussersl <58488336+mussersl@users.noreply.github.com>
2021-05-15 16:41:10 -0400

    fix1

--- Core/AllWordsParser.cs
+++ Core/AllWordsParser.cs
@@ -8,7 +8,18 @@ namespace Chatbot
     {
         
 		public List<string> parseQuestion(string question){
-			return new List<String>(question.Split(' '));
+            string temp = question.ToLower();
+			List<string> words = new List<string>(temp.Split(' '));
+            EnglishSmartStopWordFilter ssf = new EnglishSmartStopWordFilter();
+            List<string> keywords = new List<string>();
+            foreach(string s in words)
+            {
+                if (!ssf.IsStopWord(s))
+                {
+                    keywords.Add(s);
+                }
+            }
+            return keywords;
 		}
 		
     }
