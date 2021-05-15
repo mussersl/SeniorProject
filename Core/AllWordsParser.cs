35e51ff4ee67d070dc1991e05b62e1dacf85d982
mussersl <58488336+mussersl@users.noreply.github.com>
2021-05-15 16:55:23 -0400

    Removed unneeded files

--- Core/AllWordsParser.cs
+++ Core/AllWordsParser.cs
@@ -6,10 +6,22 @@ namespace Chatbot
 {
     public class AllWordsParser : KeywordParserInterface
     {
-        
-		public List<string> parseQuestion(string question){
-			return new List<String>(question.Split(' '));
-		}
-		
+
+        public List<string> parseQuestion(string question)
+        {
+            string temp = question.ToLower();
+            List<string> words = new List<string>(temp.Split(' '));
+            EnglishSmartStopWordFilter ssf = new EnglishSmartStopWordFilter();
+            List<string> keywords = new List<string>();
+            foreach (string s in words)
+            {
+                if (!ssf.IsStopWord(s))
+                {
+                    keywords.Add(s);
+                }
+            }
+            return keywords;
+        }
+
     }
 }
