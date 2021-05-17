e46d843f91bee8d34313bd83b10d881689d20160
Andrew C. Blonsky <blonskac@rose-hulman.edu>
2021-05-16 22:10:43 -0400

    updated IP Address

--- DatabaseInteraction/Database.cs
+++ DatabaseInteraction/Database.cs
@@ -14,7 +14,7 @@ namespace Chatbot
         }
         
         public void connection(){
-            string myConnectString = "server=137.112.229.102;port=3300;Database=IRPAchatbot;uid=root;pwd=test;";
+            string myConnectString = "server=192.168.1.101; port=3300;Database=IRPAchatbot;uid=root;pwd=test;";
             connect.ConnectionString = myConnectString;
             try
             {
