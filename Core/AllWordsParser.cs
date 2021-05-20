e81b592a23c105248b18f7c236d684657c121df6
mussersl <58488336+mussersl@users.noreply.github.com>
2021-05-20 15:25:55 -0400

    update parser to ignore non-alphanumeric characters

--- Core/AllWordsParser.cs
+++ Core/AllWordsParser.cs
@@ -6,11 +6,21 @@ namespace Chatbot
 {
     public class AllWordsParser : KeywordParserInterface
     {
-
         public List<string> parseQuestion(string question)
         {
-            string temp = question.ToLower();
-            List<string> words = new List<string>(temp.Split(' '));
+            char[] temp = question.ToLower().ToCharArray();
+
+            for(int i = 0; i<temp.Length, i++)
+            {
+                if (!char.IsLetterOrDigit(temp[i]))
+                {
+                    temp[i] = ' ';
+                }
+            }
+
+            string newQuestion = temp.ToString();
+
+            List<string> words = new List<string>(newQuestion.Split(' '));
             EnglishSmartStopWordFilter ssf = new EnglishSmartStopWordFilter();
             List<string> keywords = new List<string>();
             foreach (string s in words)
