decefb36e93a3efc9202ccb8bc27b6c8cb06bbe9
ChrisStout2 <72475681+ChrisStout2@users.noreply.github.com>
2021-05-23 13:28:17 -0400

    Deleted old commented out code
    Visual Studio won't let me open the project properly until I do this.

--- Core/ResponseGeneratorR1.cs
+++ Core/ResponseGeneratorR1.cs
@@ -9,27 +9,6 @@ namespace Chatbot
         public List<string> generateResponse(List<Answer> answers){
             var hold = new List<string>();
 
-			/*
-            double hold2 = -1;
-            double hold3 = -1;
-            int count = 0;
-			
-			             foreach (Answer keep in answers){
-                if(keep.relevency > hold3){
-                    hold3 = keep.relevency;
-                    hold2 = count;
-                    count += 1;
-                }
-            }
-            if(hold3 == -1){
-                hold.Add("No valid response detected");
-                return hold;
-            }
-            hold.Add(answers[(int)hold2].answer);
-            return hold;
-			*/
-
-
 
 			//Sort the list so that the most relevent answers are first
 			answers.Sort((Answer a, Answer b) => { return a.relevency.CompareTo(b.relevency); });
@@ -77,64 +56,6 @@ namespace Chatbot
 			
         }
 		
-		//Mergesort implementation originally written by Princi Singh, modified to work with answers
-		
-		public void Merge(List<Answer> answers, int l, int m, int r){
-		
-			int n1 = m - l + 1;
-        	int n2 = r - m;
-			
-		    List<Answer> L = new List<Answer>();
-       		List<Answer> R = new List<Answer>();
-        	int i, j;
-			
-			for (i = 0; i < n1; ++i)
-            	L[i] = answers[l + i];
-        	for (j = 0; j < n2; ++j)
-            	R[j] = answers[m + 1 + j];
-				
-			i = 0;
-       		j = 0;
-			
-			int k = l;
-        	while (i < n1 && j < n2) {
-            	if (L[i].relevency >= R[j].relevency) {
-                	answers[k] = L[i];
-                	i++;
-            	}
-            	else {
-                	answers[k] = R[j];
-                	j++;
-            	}
-            	k++;
-        	}
-			
-			while (i < n1) {
-            	answers[k] = L[i];
-            	i++;
-            	k++;
-        	}
-			
-			while (j < n2) {
-            	answers[k] = R[j];
-            	j++;
-            	k++;
-        	}
-			
-		}
-		
-		public void Sort(List<Answer> answers, int l, int r){
-			if (l < r) {
-           
-            	int m = l+ (r-l)/2;
- 
-            
-            	Sort(answers, l, m);
-            	Sort(answers, m + 1, r);
- 
-            	Merge(answers, l, m, r);
-        	}
-		}
 		
     }
 }
